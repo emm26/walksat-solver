f3bcb60f2602088a157ac9387106b3b22bb511eb
Nil Torrent Bureu <ntorrentbureu@gmail.com>
2019-03-13 22:09:44 +0100

    Classes turned into functions, basic walksat solver algorithm implemented.
    - All methods have been changed to functions.
    - The 'solve' function has been implemented, containing a basic implementation of the walksat solver algorithm.
    - New helper functions added to complete the 'solve' function.

--- solver.py
+++ solver.py
@@ -1,56 +1,103 @@
 import sys
 import random
 
-# Clases
+# Clases removed
 
-class RandomInterpretation():
-    """An interpretation is an assignment of the possible values to variables"""
+def get_random_interpretation(num_vars):
+    """Get a random interpretation for all the variables"""
+    interpretation = range(1, num_vars+1)
 
-    def __init__(self, num_vars):
-        self.num_vars = int(num_vars)
-        self.vars = None
-        self.get_random_interpretation() # generate a random interp. when creating the instance 
+    for i in xrange(num_vars):
+        interpretation[i] *= multiplicator() # 50/50 probability to be - or +
 
-    def get_random_interpretation(self):
-        """Get a random interpretation for all the variables"""
-        self.vars = range(1, self.num_vars+1)
+    return interpretation
 
-        for i in xrange(self.num_vars):
-            self.vars[i] *= multiplicator() # 50/50 probability to be - or +
+# Tells if the given interpretation satisfies the given formula.
+# If the formula is not safisfied it returns the first unsatisfied clause.
+def satisfies(interpretation, formula):
+    for clause in formula:
+        clause_length = len(clause)
+        for lit in clause:
+            if lit == interpretation[abs(lit)-1]: # satisfies clause
+                break
+            else:
+                clause_length -= 1
+        if clause_length == 0: # falsified clause
+            return False, clause
 
-    def satisfies(self, formula):
-        for clause in formula:
-            length = len(clause)
+    return True, []
 
-            for lit in clause:
-                if lit == self.vars[abs(lit)-1]: # satisfies clause
-                    break
-                else:
-                    length -= 1
-            if length == 0: # falsified clause
-                return False
+# Returns the number of unsatisfied clauses of a formula given an interpretation.
+def count_unsatisfiable_clauses(interpretation, formula):
+    unsatisfied_clauses = 0
 
-        return True
+    for clause in formula:
+        clause_length = len(clause)
+        for lit in clause:
+            if lit == interpretation[abs(lit)-1]: # satisfies clause
+                break
+            else:
+                clause_length -= 1
+        if clause_length == 0: # falsified clause
+            unsatisfied_clauses+=1
+
+    return unsatisfied_clauses
+
+# Given an interpretation an a variable of the interpretation, returns a copy of
+# the interpretation with the value of the variable changed. 
+def get_interpretation_with_changed_variable_sense(interpretation, variable_to_change):
+    new_clause = list(interpretation)
+    new_clause[abs(variable_to_change)-1] *= -1
+    return new_clause
+
 
 
 def multiplicator():
-	if random.random() < 0.5:
-		return 1
-	else:
-		return -1
+    if random.random() < 0.5:
+        return 1
+    return -1
 
-class Solver():
-    """docstring for [object Object]."""
-    def __init__(self):
-        self.probability = 0
-        #self.problem = problem
-    	#self.best_sol = None
-    	#self.best_cost = problem
+def flip_a_coin(probability):
+    if random.random() < probability:
+        return True
+    return False
 
-    def solve(self, formula, num_vars, max_tries = 100, rnd_walk = 0.1, max_restarts = 10):
-        random_interpretation = RandomInterpretation(num_vars)
-        print "Interpretacio random: " + str(random_interpretation.vars)
-        print random_interpretation.satisfies(formula)
+
+
+def solve(formula, num_vars, max_flips = 500, rnd_walk = 0.1, max_restarts = 50):
+    """
+    random_interpretation = RandomInterpretation(num_vars)
+    print "Interpretacio random: " + str(random_interpretation.vars)
+    print random_interpretation.satisfies(formula)
+    """
+
+    for _ in xrange(max_restarts):
+        random_interpretation = get_random_interpretation(num_vars)
+        for _ in xrange(max_flips):
+            is_satifiable, unsatisfied_clause = satisfies(random_interpretation, formula)
+            if is_satifiable:
+                return random_interpretation
+
+            best_interpretation = None
+            least_unsatisfied_clauses = sys.maxint
+            for var in unsatisfied_clause:
+                current_interpretation = get_interpretation_with_changed_variable_sense(random_interpretation, var)
+                current_unsatisfied_clauses = count_unsatisfiable_clauses(current_interpretation, formula)
+                if (current_unsatisfied_clauses < least_unsatisfied_clauses):
+                    least_unsatisfied_clauses = current_unsatisfied_clauses
+                    best_interpretation = current_interpretation
+
+            #print best_interpretation
+            #print least_unsatisfied_clauses
+
+            if least_unsatisfied_clauses > 0 and flip_a_coin(rnd_walk):
+                random_variable = random.randint(0, len(unsatisfied_clause) - 1)
+                random_interpretation = get_interpretation_with_changed_variable_sense(random_interpretation, unsatisfied_clause[random_variable])
+            else:
+                random_interpretation = best_interpretation
+
+    return "No solution found"
+
 
 # Functions
 
@@ -82,7 +129,6 @@ if __name__ == '__main__' :
     #print "Num_vars: " + num_vars
     #print formula
 
-	# Create a solver instance with the problem to solve
-    solver = Solver()
 	# Solve the problem and get the best solution found
-    best_sol = solver.solve(formula, num_vars)
+    best_sol = solve(formula, int(num_vars))
+    print best_sol
